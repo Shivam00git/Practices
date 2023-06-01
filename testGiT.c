4680cb3deedf139fe3fc1f73174b441706d7601a
Shivam00git <135209521+Shivam00git@users.noreply.github.com>
2023-06-01 13:12:10 +0530

    Add via phone
    Only for testing

---
+++ testGiT.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int x,y;
+
+    printf(" enter any number:");
+    scanf("%d",&x);
+    if(x%2==0)
+    printf("number is even");
+    else
+    printf("number is odd");
+    return 0;
+}
